32e83c590e51eaff28234fd5e8d1a4dfb29b4cde
Alioyk <aliokyeskentang@gmail.com>
2025-10-01 21:34:28 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++ (1)/Code/lat2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+string angka[] = {"nol", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"};
+string anu(int x) {
+    if (x < 12) {
+        return angka[x];
+    } else if (x < 20) {
+        return angka[x-10] + " belas";
+    } else if (x < 100 && x%10 == 0) {
+        return angka[x/10] + " puluh";
+    } else if (x < 100) {
+        return angka[x/10] + " puluh " + angka[x%10];
+    } else if (x == 100) {
+        return "seratus";
+    }
+    return "";
+}
+int main() {
+    int x;
+    cin >> x;
+    cout << x << " : " << anu(x) << endl;
+}
